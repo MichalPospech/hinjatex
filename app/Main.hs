e7fbe6983d273b02f0e892c09a080680adb18843
Michal Pospěch <michal.pospech@gmail.com>
2021-10-30 20:36:52 +0000

    Arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Options.Applicative (argument, help, metavar, progDesc, str, execParser, info, fullDesc)
+import Options.Applicative.Types (Parser)
+import Options.Applicative.Common (runParser)
+
+data Arguments = Arguments
+  { json :: String,
+    latex :: String
+  }
+
+jsonFile :: Parser String
+jsonFile = argument str (metavar "JSON" <> help "Path to JSON datafile")
+
+latexTemplate :: Parser String
+latexTemplate = argument str (metavar "LATEX" <> help "Path to LaTeX template")
+
+parser = Arguments <$> jsonFile <*> latexTemplate
+
+
 main :: IO ()
-main = putStrLn "Hello Hask"
+main = do
+    args <- execParser $ info parser fullDesc 
+    return ()
